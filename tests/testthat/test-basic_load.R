4d1f140ec828303e8d0dbbd8e4670e6bef7ba9ab
Cam Race <52536248+cjrace@users.noreply.github.com>
2025-03-25 03:41:54 +0000

    attempt to make the tests more stable

--- tests/testthat/test-basic_load.R
+++ tests/testthat/test-basic_load.R
@@ -1,8 +1,12 @@
 # Start an app running ========================================================
 app <- AppDriver$new(
   name = "basic_load_nav",
-  expect_values_screenshot_args = FALSE
+  expect_values_screenshot_args = FALSE,
+  load_timeout = 45 * 1000,
+  timeout = 20 * 1000
 )
+
+app$wait_for_idle(2000)
 
 test_that("App loads and title of app appears as expected", {
   expect_equal(app$get_text("title"), "Explore education statistics analytics")
